2ad56b2f9bb269e5eaec94c019a2c227f9c0c7ee
mmarcot <mmarcot@gmail.com>
2014-04-06 19:06:14 +0200

    changement de liens sur la page d'index en fonction de la connexion

--- index.php
+++ index.php
@@ -28,12 +28,26 @@ if(!empty($_SESSION['erreur'])){
   </p>
 </form>
 
-<table style='margin:auto; background:#EEEEEE; padding:8px'>
-<tr><td><a style='text-align:center;' href='connexion.php'>Me connecter</a></td></tr>
-<tr><td><a style='text-align:center;' href='inscription.php'>M'inscrire</a></td></tr>
-</table>
-
 <?php
+// si l'utilisateur est connecté :
+if( !empty($_SESSION['connex_active'] )) {
+  echo <<<CONN
+  <table style='margin:auto; background:#EEEEEE; padding:8px'>
+  <tr><td><a style='text-align:center;' href='mon_compte.php'>Mon compte</a></td></tr>
+  <tr><td><a style='text-align:center;' href='deconnexion.php'>Me deconnecter</a></td></tr>
+  </table>
+CONN;
+
+}
+else { // sinon il n'est pas connecté :
+  echo <<<PACO
+  <table style='margin:auto; background:#EEEEEE; padding:8px'>
+  <tr><td><a style='text-align:center;' href='connexion.php'>Me connecter</a></td></tr>
+  <tr><td><a style='text-align:center;' href='inscription.php'>M'inscrire</a></td></tr>
+  </table>
+PACO;
+}
+
 finHTML();
 ?>
 
