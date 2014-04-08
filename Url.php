9a56b214d1b59ec60efe298615fcdd39a0019770
mmarcot <mmarcot@gmail.com>
2014-04-08 20:46:53 +0200

    Squelette classe Url + interface des pages mes_liens.php et administration.php

---
+++ Url.php
@@ -0,0 +1,94 @@
+<?php
+class Url  {
+  /**
+   * Methode qui permet de verifier si un URL entre
+   * est valide ou non
+   */
+  public static function verifier($url) {
+    $etatSyn = true;
+    $etatEx = true;
+    $etatCib = true;
+    
+    $etatSyn = verifierSynthaxe($url);
+    $etatEx = verifierExisteDeja($url);
+    $etatCib = verifierCible($url);
+
+    if( $etatSyn && $etatEx && $etatCib )
+      return true;
+    else 
+      return false;
+  }
+
+
+  /**
+   * Methode qui verifie la synthaxe de l'url
+   */
+  public static function verifierSynthaxe($url) {
+    $etat = true;
+    
+    /* TODO verif ici */
+
+    return $etat;
+  }
+
+
+  /**
+   * Methode qui verifie si l'url existe deja dans la BDD
+   */
+  public static function verifierExisteDeja($url) {
+    $etat = true;
+    
+    /* TODO verif ici */
+
+    return $etat;
+  }
+
+
+  /**
+   * Methode qui verifier si l'url cible est valide càd qu'il
+   * ne pointe pas vers un autre url reduit par exemple
+   */
+  public static function verifierCible($url) {
+    $etat = true;
+    
+    /* TODO verif ici */
+
+    return $etat;
+  }
+
+
+
+  /**
+   * Methode qui genere une url courte à partir de
+   * l'url original
+   */
+  public static function genererUrlCourt($url_orig) {
+    /* TODO completer */
+  }
+
+
+  /**
+   * Methode qui renvoie tous les url créés par l'auteur
+   * passé en parametre
+   */
+  public static function getUrlByAuthor($author) {
+    /* TODO completer */
+  }
+
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ mes_liens.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h2 style='text-align:center'>Mes liens</h2>";
+
+echo "<table style='margin:auto; background:#EEEEEE; padding:8px'>";
+echo "<tr><td><a style='text-align:center;' href='mon_compte.php'>Mon compte</a></td></tr>";
+echo "<tr><td><a style='text-align:center;' href='index.php'>Retour accueil</a></td></tr>";
+echo "<tr><td><a style='text-align:center;' href='deconnexion.php'>Me deconnecter</a></td></tr>";
+echo "</table>";
+?>
