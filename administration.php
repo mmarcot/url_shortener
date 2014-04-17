af490d05067d569c20055f3627ec1926c3a6926b
vvantoan <van_toan@live.fr>
2014-04-17 16:20:31 +0200

    modification de la redirection de la suppression de lien dans la page administrateur

--- administration.php
+++ administration.php
@@ -73,7 +73,7 @@ if( Membre::estAdmin($_SESSION['connex_active']) ) {
             <td>";
     echo Membre::getPseudoFromId($ligne->auteur) . "</td>";
    //echo "<td><input type='checkbox' name='${res->id}' value='suppr' style='margin:auto; display:block;'></td>";
-	echo "<td><a href='suppr_liens.php?id=" .$ligne->id . "'> supprimer </a>";
+	echo "<td><a href='suppr_liens2.php?id=" .$ligne->id . "'> supprimer </a>";
     echo "</tr>";
   }
   echo "</table>";
