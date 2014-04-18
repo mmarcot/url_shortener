dcc1dd5dce0bacefb9399c8c47d0faf0fe8b7b21
mmarcot <mmarcot@gmail.com>
2014-04-18 09:40:34 +0200

    ajout de la classe Utilisation

---
+++ Utilisation.php
@@ -0,0 +1,16 @@
+<?php 
+
+/**
+ * Classe permettant d'interfacer la table Utilisations
+ */
+class Utilisation {
+
+	/**
+	 * Methode qui permet d'ajouter une utilisation pour un lien
+	 */
+	public static function ajouterUtilisation($id_url) {
+		//TODO completer
+	}
+
+}
+?>
