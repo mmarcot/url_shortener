636c66dcd9a7487ee002b9df30af4fca0e96924a
mmarcot <mmarcot@gmail.com>
2014-04-18 10:25:24 +0200

    Codage de methode permettant de compter les utilisations

--- Utilisation.php
+++ Utilisation.php
@@ -1,4 +1,5 @@
 <?php 
+include_once("config.php");
 
 /**
  * Classe permettant d'interfacer la table Utilisations
@@ -9,8 +10,41 @@ class Utilisation {
 	 * Methode qui permet d'ajouter une utilisation pour un lien
 	 */
 	public static function ajouterUtilisation($id_url) {
-		//TODO completer
+		global $pdo;
+  
+    $req_ins = $pdo->prepare("INSERT INTO utilisations(url, date) VALUES(:url, :dat);");
+    $req_ins->bindParam(":url", $id_url);
+    $req_ins->bindParam(":dat", date());
+    $req_ins->execute();
 	}
+
+
+	/**
+   * Methode qui permet de compter le nombre d'utilisation dans la BDD
+   */
+  public static function countAll() {
+    global $pdo;
+    
+    $req_cmpt = $pdo->prepare("SELECT * FROM utilisations");
+	  $req_cmpt->execute();
+    
+    return ($req_cmpt->rowCount());
+  }
+
+
+  /**
+   * Methode qui permet de compter le nombre d'utilisation dans la BDD
+   * par url courte (id)
+   */
+  public static function countByUrl($id_url) {
+    global $pdo;
+    
+    $req_cmpt = $pdo->prepare("SELECT * FROM utilisations WHERE url=:url");
+    $req_cmpt->bindParam(':url', $id_url, PDO::PARAM_INT);
+	  $req_cmpt->execute();
+    
+    return ($req_cmpt->rowCount());
+  }
 
 }
 ?>
