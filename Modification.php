c5ad5e7a92550c1a89a6f9b40705a2f1e152ce36
vvantoan <van_toan@live.fr>
2014-04-21 14:23:45 +0200

    rectification des erreurs de syntaxe

--- Modification.php
+++ Modification.php
@@ -16,6 +16,7 @@ class Modification {
 	
 		$req_modif_pseudo = $pdo->prepare("UPDATE `membres` SET `pseudo`=:NPseudo WHERE id=:id");
 		$req_modif_pseudo->bindParam(':NPseudo', $NPseudo);
+		$req_modif_pseudo->bindParam(':id', $id);
 		$req_modif_pseudo->execute();
 		$req_modif_pseudo->setFetchMode(PDO::FETCH_OBJ);
 	}
@@ -26,7 +27,7 @@ class Modification {
 		$req_modif_nom = $pdo->prepare("UPDATE `membres` SET `nom`=:NNom WHERE id=:id");
 		$req_modif_nom->bindParam(':NNom', $NNom);
 		$req_modif_nom->execute();
-		$req_modif_nom->setFetchMode(PDO::FETCH_OBJ)
+		$req_modif_nom->setFetchMode(PDO::FETCH_OBJ);
 	
 	}
 	
@@ -36,16 +37,16 @@ class Modification {
 		$req_modif_prenom = $pdo->prepare("UPDATE `membres` SET `prenom`=:NPrenom WHERE id=:id");
 		$req_modif_prenom->bindParam(':NPrenom', $NPrenom);
 		$req_modif_prenom->execute();
-		$req_modif_prenom->setFetchMode(PDO::FETCH_OBJ)
+		$req_modif_prenom->setFetchMode(PDO::FETCH_OBJ);
 	}
-	
+
 	public static function modifEmail($id, $NEmail) {
 		global $pdo;
 	
 		$req_modif_email = $pdo->prepare("UPDATE `membres` SET `email`=:NEmail WHERE id=:id");
 		$req_modif_email->bindParam(':$NEmail', $NEmail);
 		$req_modif_email->execute();
-		$req_modif_email->setFetchMode(PDO::FETCH_OBJ)
+		$req_modif_email->setFetchMode(PDO::FETCH_OBJ);
 	}
 	
 	public static function modifProfil($NProfil) {
@@ -54,7 +55,7 @@ class Modification {
 		$req_modif_profil = $pdo->prepare("UPDATE `membres` SET `prefil`=:NProfil WHERE id=:id");
 		$req_modif_profil->bindParam(':$NProfil', $NProfil);
 		$req_modif_profil->execute();
-		$req_modif_profil->setFetchMode(PDO::FETCH_OBJ)
+		$req_modif_profil->setFetchMode(PDO::FETCH_OBJ);
 	}
 }
 
