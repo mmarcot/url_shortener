9a2d6aebcbb5853ce883d2c316151f8911881035
vvantoan <van_toan@live.fr>
2014-04-21 13:45:44 +0200

    prototype des methodes de modification

---
+++ Modification.php
@@ -0,0 +1,44 @@
+<?php
+include_once("config.php");
+include_once("tools.php");
+
+/**
+ * Classe qui regroupe un ensemble de methodes statiques
+ * pour la gestion des membres 
+ */
+class Modification {
+
+	///////////////////////////////////////////////////////////////////////
+	// MODIFICATION														///
+	///////////////////////////////////////////////////////////////////////
+	public static function modifPseudo() {
+	}
+	
+	public static function modifNom() {
+	}
+	
+	public static function modifPrenom() {
+	}
+	
+	public static function modifEmail() {
+	}
+	
+	public static function modifProfil() {
+	}
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
