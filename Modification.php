8b5493a74398fbfc03b2fb02069c9a6cfe0061bb
vvantoan <van_toan@live.fr>
2014-04-21 13:58:32 +0200

    codage de la methode de modification du pseudo

--- Modification.php
+++ Modification.php
@@ -11,19 +11,25 @@ class Modification {
 	///////////////////////////////////////////////////////////////////////
 	// MODIFICATION														///
 	///////////////////////////////////////////////////////////////////////
-	public static function modifPseudo() {
+	public static function modifPseudo($NPseudo) {
+		global $pdo;
+	
+		$req_modif_pseudo = $pdo->prepare("UPDATE `membres` SET `pseudo`=:NPseudo WHERE id=:id");
+		$req_modif_pseudo->bindParam(':NPseudo', $NPseudo);
+		$req_modif_pseudo->execute();
+		$req_modif_pseudo->setFetchMode(PDO::FETCH_OBJ);
 	}
 	
-	public static function modifNom() {
+	public static function modifNom($id) {
 	}
 	
-	public static function modifPrenom() {
+	public static function modifPrenom($id) {
 	}
 	
-	public static function modifEmail() {
+	public static function modifEmail($id) {
 	}
 	
-	public static function modifProfil() {
+	public static function modifProfil($id) {
 	}
 }
 
