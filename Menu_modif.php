53f548809aad6bb28e38633a9844783940c13996
vvantoan <van_toan@live.fr>
2014-04-21 15:12:13 +0200

    essai pour recuperer les valeurs

--- Menu_modif.php
+++ Menu_modif.php
@@ -2,8 +2,9 @@
 include_once("administration.php");
 include_once("Modification.php");
 
-//TODO verifier que le lien appartient bien au pseudo connecté
 
-Modification::modifPseudo('23', 'modif');
-header("Location: administration.php");
+		Modification::modifPseudo('23', 'bari');
+		header("Location: administration.php");
+	
+
 ?>
