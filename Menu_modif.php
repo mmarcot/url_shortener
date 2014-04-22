2fe23957216ba1d66e6a2144e0fffb37fd641efd
vvantoan <van_toan@live.fr>
2014-04-22 09:20:11 +0200

    appel de toutes les methodes de modification

--- Menu_modif.php
+++ Menu_modif.php
@@ -2,9 +2,23 @@
 include_once("administration.php");
 include_once("Modification.php");
 
-
-		Modification::modifPseudo('23', 'bari');
-		header("Location: administration.php");
+	if((!empty($_POST['id_m']))&&(!empty($_POST['pseudo_m']))) {
+		Modification::modifPseudo($_POST['id_m'], $_POST['pseudo_m']);
+		}
+	if((!empty($_POST['id_m']))&&(!empty($_POST['nom_m']))) {
+		Modification::modifNom($_POST['id_m'], $_POST['nom_m']);
+	}
+	if((!empty($_POST['id_m']))&&(!empty($_POST['prenom_m']))) {
+		Modification::modifPrenom($_POST['id_m'], $_POST['prenom_m']);
+	}
+	if((!empty($_POST['id_m']))&&(!empty($_POST['email_m']))) {
+		Modification::modifEmail($_POST['id_m'], $_POST['email_m']);
+	}
+	if((!empty($_POST['id_m']))&&(!empty($_POST['profil_m']))) {
+		Modification::modifProfil($_POST['id_m'], $_POST['profil_m']);
+	}
+	
+	header("Location: administration.php");
 	
 
 ?>
