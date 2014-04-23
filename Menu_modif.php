159621eb5ef605f8335c5ff7f9e15b8fbe782e3a
vvantoan <van_toan@live.fr>
2014-04-23 12:48:17 +0200

    cristalisation de la modification des membres

--- Menu_modif.php
+++ Menu_modif.php
@@ -1,6 +1,13 @@
 <?php
 include_once("Modification.php");
 	
+	$pseudo = Modification::getPseudoFromId($_GET['id']);
+	$nom = Modification::getNomFromId($_GET['id']);
+	$prenom = Modification::getPrenomFromId($_GET['id']);
+	$email = Modification::getEmailFromId($_GET['id']);
+	$profil = Modification::getProfilFromId($_GET['id']);
+	
+	
 	echo "
 	<h2 style='text-align:center;'>MODIFICATION</h2>
 	<form action='Menu_modif.php' method='post' accept-charset='utf-8'>
@@ -11,31 +18,36 @@ include_once("Modification.php");
 		</tr>
 		<tr>
 		  <td>Pseudo :</td>
-		  <td><input type='text' name='pseudo_m' value=''></td>
+		  <td><input type='text' name='pseudo_m' value='$pseudo'></td>
 		</tr>
 		<tr>
 		  <td>Nom :</td>
-		  <td><input type='text' name='nom_m' value=''></td>
+		  <td><input type='text' name='nom_m' value='$nom'></td>
 		</tr>
 		<tr>
 		  <td>Prenom :</td>
-		  <td><input type='text' name='prenom_m' value=''></td>
+		  <td><input type='text' name='prenom_m' value='$prenom'></td>
 		</tr>
 		<tr>
 		  <td>Email :</td>
-		  <td><input type='text' name='mail_m' value=''></td>
+		  <td><input type='text' name='mail_m' value='$email'></td>
 		</tr>
-		
-	  <p style='text-align:center;'>
-		<input type='submit' value='Modifier &rarr;'>
-	  </p>
-	</form>
+		<tr>
+			<td>Profil :</td>
+			<td>
+			<input type = 'radio' name = 'choix' value = 'administrateur'/>
+			Administrateur 
+			
+			<input type = 'radio' name = 'choix' value = 'membre'/>
+			Membre											
+			</td>
+		</tr>
+	</table>
 	
 	<p style='text-align:center;'>
-	<select name = 'profil'>
-	<option>membre</option>
-	<option>administrateur</option>
-	</select>";
+		<input type='submit' value='Modifier &rarr;'>
+	  </p>
+	</form>";
 	
 	echo"<p style='text-align:center;'>
 	<a href='administration.php'>retour</a>";
